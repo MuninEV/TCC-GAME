be21e256321c95338ee353c3d6c226f6ef400943
MuninEV <nadiaevellin2005@hotmail.com>
2022-10-10 19:05:25 -0400

    configurando trestle e criando tela de cadastro de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum :tipo_usuario, {administrador: 0, jogador: 1}
 end
