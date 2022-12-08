924bf04db62c62322b2e5d8c74f15155a40c640f
gfernandes4 <gabriel01052004@gmail.com>
2022-12-08 17:05:54 -0300

    pontuação-para-ranking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_215013) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_214919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_215013) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "pontuacaos", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.float "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pontuacaos_on_user_id"
+  end
+
   create_table "questao_alternativas", force: :cascade do |t|
     t.bigint "questao_id", null: false
     t.bigint "alternativa_id", null: false
@@ -44,6 +52,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_215013) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["dificuldade_id"], name: "index_questoes_on_dificuldade_id"
+  end
+
+  create_table "tabela_pontuacaos", force: :cascade do |t|
+    t.string "jogador"
+    t.string "pontuacao"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tentativa_questaos", force: :cascade do |t|
@@ -75,6 +90,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_215013) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pontuacaos", "users"
   add_foreign_key "questao_alternativas", "alternativas"
   add_foreign_key "questao_alternativas", "questoes", column: "questao_id"
   add_foreign_key "questoes", "dificuldades"
