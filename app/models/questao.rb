1b51db21c3365f1859639657384038f913c0d5c3
gfernandes4 <gabriel01052004@gmail.com>
2022-12-13 17:41:03 -0300

    add regras de excluir questao

--- app/models/questao.rb
+++ app/models/questao.rb
@@ -4,6 +4,8 @@ class Questao < ApplicationRecord
 
   has_many  :questao_alternativas
   has_many  :alternativas, through: :questao_alternativas, dependent: :delete_all
+  has_many  :tentativa_questaos
+  has_many  :tentativas, through: :tentativa_questaos, dependent: :delete_all
   
   validates :enunciado, presence: true
 end
