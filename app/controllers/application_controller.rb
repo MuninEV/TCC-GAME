eadeee86f6b618c54393618e5963d1c25c3708cf
MuninEV <nadiaevellin2005@hotmail.com>
2022-12-13 16:16:35 -0400

    fazendo algumas alterações no layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :user_admin?
 
   def usuario_logado?
     redirect_to users_url if !user_signed_in?
+  end
+
+  def user_admin?
+    if current_user&.adm? 
+      redirect_to "/apollo"
+    end
   end
 
   protected
